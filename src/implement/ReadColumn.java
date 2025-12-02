59d81a1de09a78e99e4a61ac2bd266444f60629e
xodbs126 <dltvmsskd@naver.com>
2025-12-02 17:50:27 +0900

    feat: 10798. 세로읽기

---
+++ src/implement/ReadColumn.java
@@ -0,0 +1,39 @@
+package implement;
+
+import java.util.Scanner;
+
+public class ReadColumn {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        Solution.solution(sc);
+    }
+
+    private static class Solution {
+        public static void solution(Scanner sc) {
+            Character[][] arr = new Character[5][15];
+            for (int i = 0; i < 5; i++) {
+                for (int j = 0; j < 15; j++) {
+                    arr[i][j] = '*';
+                }
+            }
+
+
+            for (int i = 0; i < 5; i++) {
+                String inputs = sc.nextLine();
+                for (int j = 0; j < inputs.length(); j++) {
+                    arr[i][j] = inputs.charAt(j);
+                }
+            }
+
+
+            for (int i = 0; i < 15; i++) {
+                for (int j = 0; j < 5; j++) {
+                    if (arr[j][i] != '*') {
+                        System.out.print(arr[j][i]);
+                    }
+                }
+            }
+        }
+    }
+}
